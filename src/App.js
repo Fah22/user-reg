9dac6f051e7b7a794e25e2d3289f55d5b8e106bf
Fadziso Gabrielle <fadziso@novodigital.biz>
2022-01-04 21:16:03 +0200

    Modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import { Formik } from 'formik'
+import * as yup from 'yup';
 
 function App() {
+
+  const Schema = yup.object().shape({
+    email: yup.string().email().required,
+    password: yup.string().required(),
+    passwordConfirmation: yup.string().required().oneOf([yup.ref('password'), 'Passwords must match'])
+  })
   return (
     <>
       <Formik initialValues={{
@@ -9,26 +16,48 @@ function App() {
               password: '',
               passwordConfirmation: ''
             }}
+            validationSchema={Schema}
             onSubmit={(values) => {
               console.log({values})
             }}>
-            {({ values, handleSubmit, handleBlur, isValid, errors, touched}) => (
-              <form>
+            {({ values, handleSubmit, handleBlur, handleChange, isValid, errors, touched}) => (
+              <form onSubmit={handleSubmit}>
                   <input name='Username'
                         placeholder='username'
+                        value={values.username}
+                        onChange={(_, event) => {
+                          handleChange(event)
+                        }}
+                        onBlur={handleBlur}
                   />
                   <input name='email'
                         placeholder='Email'
+                        value={values.email}
+                        onChange={(val, event) => {
+                          handleChange(event)
+                        }}
+                        onBlur={handleBlur}
                   />
                   <input name='password'
                         placeholder='Password'
+                        value={values.password}
+                        onChange={(val, event) => {
+                          handleChange(event)
+                        }}
+                        onBlur={handleBlur}
                   />
                   <input name='passwordConfirmation'
                         placeholder='Re-enter password'
+                        value={values.passwordConfirmation}
+                        onChange={(val, event) => {
+                          handleChange(event)
+                        }}
+                        onBlur={handleBlur}
                   />
+                  <button type='submit'
+                          disabled={!isValid}>Register</button>
                </form>
             )}
-      
       </Formik>
     </>
   );
